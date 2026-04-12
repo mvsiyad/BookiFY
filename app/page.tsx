2be5d043ebfd43c81e8739ef41094e56e6fb27bb
mziyad10 <mvsiyad10@gmail.com>
2026-04-12 21:38:51 +0530

    feat:completed homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import BookCard from "@/components/BookCard";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
 
 const page = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <main className="wrapper container">
+      <HeroSection />
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+        ))}
+      </div>
+    </main>
+  );
+};
 
-export default page
+export default page;
