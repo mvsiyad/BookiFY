a2871fad541052e481a9f107b9bd6443cf83a946
mziyad10 <mvsiyad10@gmail.com>
2026-04-18 21:19:23 +0530

    feat:completed database &schema

---
+++ database/mongoose.ts
@@ -0,0 +1,39 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+const cached =
+  global.mongooseCache ||
+  (global.mongooseCache = { conn: null, promise: null });
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+  console.info("Connected to MongoDB");
+  return cached.conn;
+};
