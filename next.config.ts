235b7cfd3f49edd4284a6e8a11518994273bd1de
mziyad10 <mvsiyad10@gmail.com>
2026-08-16 21:37:30 +0530

    feat:fetch book completed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'covers.openlibrary.org',
-      },
-    ],
-  },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '100mb',
+        }
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: { remotePatterns: [
+            { protocol: 'https', hostname: 'covers.openlibrary.org' },
+            { protocol: 'https', hostname: 'lspfdyhgsrgsxcju.public.blob.vercel-storage.com' },
+        ]}
 };
 
 export default nextConfig;
